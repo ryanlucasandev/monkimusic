dad5fb4d8784156497aad7d78aa287f1f808777c
ryanlucasandev <ryan.lucasan.dev@gmail.com>
2026-06-09 15:06:27 +0800

    bug fix: fix to app crashing due to audio service requesting permissions

--- android/app/src/main/kotlin/com/monkimusic/monkimusic/MainActivity.kt
+++ android/app/src/main/kotlin/com/monkimusic/monkimusic/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.monkimusic.monkimusic
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity(){
+    
+}
