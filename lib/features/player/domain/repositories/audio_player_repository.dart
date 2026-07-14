f9dedbdbaf578e16961b84ed140e1a00d4168f25
ryanlucasandev <ryan.lucasan.dev@gmail.com>
2026-07-15 01:49:22 +0800

    remove unused repository

--- lib/features/player/domain/repositories/audio_player_repository.dart
+++ lib/features/player/domain/repositories/audio_player_repository.dart
@@ -3,7 +3,6 @@ import 'package:monkimusic/features/player/domain/entities/songs_entity.dart';
 
 abstract class AudioPlayerRepository {
   Future<List<MediaItem>> getQueue();
-  Future<List<MediaItem>> songsEntityToMediaItem(List<SongsEntity> songs);
   Future<void> initSongs(List<SongsEntity> songs);
   Future<void> loadTrack(int index);
   Future<void> seek(Duration position);
